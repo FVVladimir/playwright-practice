921861531c717c64403a48a2226d0aa1e5753a54
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-12 22:00:15 +0300

    create new file

---
+++ page-obgect/forms/signUpForm.ts
@@ -0,0 +1,16 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class SignUpForm {
+  readonly page: Page;
+  readonly signInButton: Locator;
+  
+
+  constructor(page: Page) {
+    this.page = page;
+    this.signInButton = page.getByRole('button', { name: 'Sign In' })
+  }
+
+  async clickSignUpButton() {
+    await this.signInButton.click()
+  } 
+}
