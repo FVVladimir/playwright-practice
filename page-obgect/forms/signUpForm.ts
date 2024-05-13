bca908f2df850329375a55e0f387fa2a1366f457
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-13 21:53:21 +0300

    edit file

--- page-obgect/forms/signUpForm.ts
+++ page-obgect/forms/signUpForm.ts
@@ -2,15 +2,29 @@ import { type Locator, type Page } from '@playwright/test';
 
 export class SignUpForm {
   readonly page: Page;
-  readonly signInButton: Locator;
-  
+  readonly buttonClose: Locator;
+  readonly fieldName: Locator;
+  readonly fieldLastName: Locator;
+  readonly fieldEmail: Locator;
+  readonly fieldPassword: Locator;
+  readonly fieldRepeatPassword: Locator;
+  readonly buttonRegister: Locator;  
 
   constructor(page: Page) {
     this.page = page;
-    this.signInButton = page.getByRole('button', { name: 'Sign In' })
+    this.buttonClose = page.locator('.close')
+    this.fieldName = page.locator('#signupName')
+    this.fieldLastName = page.locator('#signupLastName')
+    this.fieldEmail = page.locator('#signupEmail')
+    this.fieldPassword = page.locator('#signupPassword')
+    this.fieldRepeatPassword = page.locator('#signupRepeatPassword')
+    this.buttonRegister = page.getByText('Register')
   }
 
-  async clickSignUpButton() {
-    await this.signInButton.click()
+  async clickButtonClose() {
+    await this.buttonClose.click()
+  } 
+  async clickRegisterButton() {
+    await this.buttonRegister.click()
   } 
 }
