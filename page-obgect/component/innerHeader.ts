3d84473c4db63b2fa98c2191dd98070912f9d4ef
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-13 21:48:23 +0300

    added new file

---
+++ page-obgect/component/innerHeader.ts
@@ -0,0 +1,21 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class InnerHeader {
+  readonly page: Page;
+  readonly dropDownMenu: Locator;
+  readonly optionSetting: Locator;
+  
+
+  constructor(page: Page) {
+    this.page = page;
+    this.dropDownMenu = page.locator('#userNavDropdown')
+    this.optionSetting = page.getByRole('link', { name: 'Settings', exact: true })
+  }
+
+  async clickDropDownMenu() {
+    await this.dropDownMenu.click()
+  } 
+  async clickOptionSetting() {
+    await this.optionSetting.click()
+  } 
+}
