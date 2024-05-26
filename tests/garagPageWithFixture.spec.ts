946420528d8efa9aab26033b70c95ccdcc2aab42
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-26 19:41:25 +0300

    add files with used fixture

--- tests/garagPageWithFixture.spec.ts
+++ tests/garagPageWithFixture.spec.ts
@@ -1,30 +1,51 @@
 import { expect } from "@playwright/test";
-import { test } from "../fixture/garagFixture";
+import { test } from "../fixture/garageFixture";
 
-import { MainPage } from "../page-obgect/pages/mainPage";
-import { SignUpForm } from "../page-obgect/forms/signUpForm";
+import { MainPage } from "../page-object/pages/mainPage";
+import { SignUpForm } from "../page-object/forms/signUpForm";
+
+let mainPage : MainPage
+let signUpForm : SignUpForm
 
 
-test.describe(" test garag page with fixture", () => {
+test.beforeEach(async({ page }) => {
 
-    let mainPage : MainPage
-    let signUpForm : SignUpForm
-         
-    test.beforeEach(async({ garagePage, page }) => {
+   mainPage = new MainPage(page)
+   signUpForm = new SignUpForm(page)
+     
+     await page.goto("/")
+     await mainPage.clickSignUpButton()        
+     await expect(page.getByText('Registration')).toBeVisible()      
+  })
 
-          mainPage = new MainPage(page)
-          signUpForm = new SignUpForm(page)
+   // test.afterAll(async () => {
+      
+   // })
 
-            
-            await page.goto("/")
-            await mainPage.clickSignUpButton()        
-            await expect(page.getByText('Registration')).toBeVisible()
-            await signUpForm.fieldName.fill('Eric')
-            await signUpForm.fieldLastName.fill('Cartman')
-            await signUpForm.fieldEmail.fill('emir+aqa@ua.fm')
-            await signUpForm.fieldPassword.fill(process.env.USER_CORRECT_PASSWORD ?? '')
-            await signUpForm.fieldRepeatPassword.fill('Qwerty12345')
-            await signUpForm.buttonRegister.click()
-            await expect(page).toHaveURL('https://qauto.forstudy.space/panel/garage')
-         })
+
+test.describe(" test garage page with fixture", () => {       
+
+        test('test registration with correct data', async ({ page }) => {      
+        
+          await signUpForm.fieldName.fill('Eric')
+          await signUpForm.fieldLastName.fill('Cartman')
+          await signUpForm.fieldEmail.fill('emir+aqa@ua.fm')
+          await signUpForm.fieldPassword.fill(process.env.USER_CORRECT_PASSWORD ?? '')
+          await signUpForm.fieldRepeatPassword.fill('Qwerty12345')
+          await signUpForm.buttonRegister.click()
+          await expect(page).toHaveURL('https://qauto.forstudy.space/panel/garage')          
+      })
+     
+         test('check the text', async ({ garagePage  }) => {        
+         await garagePage.text.toBeVisible()
+      })
+      //    test('check the add car button', async ({ page }) => {        
+        
+      // })
+      //    test('check the garage link', async ({ page }) => {        
+          
+      // })
+
+      //   test('check th profile link', async ({ page }) => {         
+      // })
 })
