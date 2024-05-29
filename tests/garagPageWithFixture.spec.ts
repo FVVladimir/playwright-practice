1fdd8a77b302c56c13d809b4ab13480ab8715308
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-05-29 20:27:56 +0300

    added test add car

--- tests/garagPageWithFixture.spec.ts
+++ tests/garagPageWithFixture.spec.ts
@@ -15,4 +15,17 @@ test.describe(" test garage page with fixture", () => {
            test('check the profile link', async ({ garagePage  }) => {        
             await expect(garagePage.profileLink).toBeVisible()
            })    
+           test.only('add car', async ({garagePage}) => { 
+
+            await garagePage.addCarButton.click()           
+            await garagePage.getByLabel('Brand').selectOption('1: 2')
+            // await garagePage.getByLabel('Model').selectOption('13: 9');
+            // await garagePage.getByLabel('Mileage').click();
+            // await garagePage.getByLabel('Mileage').fill('12345');
+            // await garagePage.getByRole('button', { name: 'Add' }).click();
+            // await page.getByRole('button', { name: '' }).click();
+            // await page.getByRole('button', { name: 'Remove car' }).click();
+            // await page.getByRole('button', { name: 'Remove' }).click();
+
+           })    
 })
