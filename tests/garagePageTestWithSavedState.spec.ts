3ecec935e1d8eb166456dd5101085f9b265490e4
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-06-13 21:41:33 +0300

    fix some errors

--- tests/garagePageTestWithSavedState.spec.ts
+++ tests/garagePageTestWithSavedState.spec.ts
@@ -1,37 +1,29 @@
 import { test, expect } from "@playwright/test"
-import { describe } from "node:test"
-
 import { GaragePage } from "../page-object/pages/garagePage"
 
-
-describe("garagePage test with saved state", () => {
+test.describe("garagePage test with saved state", () => {
 
     let garagePage: GaragePage
-    
-    test.use({ storageState: "../../userState.json" })
-    
-    test.beforeEach(async ({page}) => {
 
+    test.use({ storageState: "./userState.json" })
+    
+    test.beforeEach(async ({page}) => {        
+       
+        garagePage = new GaragePage(page)
         await page.goto("/panel/garage")
-
+        
     })
 
-    test('check the text on garage page', async ({ page }) => {
-
-        garagePage = new GaragePage(page)
+    test('check the text on garage page', async () => {   
 
         await expect(garagePage.text).toBeVisible()
     })
 
-    test('check the garage link', async ({ page }) => {
-
-        garagePage = new GaragePage(page)
+    test('check the garage link', async () => {
         await expect(garagePage.garageLink).toBeVisible()
     })
 
-    test('check the profile link', async ({ page }) => {
-
-        garagePage = new GaragePage(page)
+    test('check the profile link', async () => {
         await expect(garagePage.profileLink).toBeVisible()
     })
 })
